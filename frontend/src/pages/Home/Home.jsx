b0e10419a68c576dd6208e045e1c85fefe8ef023
Ollia0 <dodo.diallo@gmail.com>
2025-04-02 14:32:58 +0200

    ETQDev: ajouter éléments à Home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,22 +1,83 @@
 import logo from './logo.svg';
 import './Home.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Home() {
+  // const api_key = "15d2ea6d0dc1d476efbca3eba2b9bbfb"
+
+  const [movieName, setMovieName] = useState('')
+  // const [movies, setMovies] = useState('')
+  
+  // update le nom du film selon l'input
+  const movieNameChange = (Event) => {
+    setMovieName(Event.target.value);
+  }
+
+  // récupère les 20 films les plus populaires
+  const getPopularMovies = async () => {
+    try {
+      const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_API_KEY}&page=1`);
+      return response.data;
+    }
+    catch (error){
+      console.log("Erreur de chargement des films");
+      return []
+    }
+  };
+
+  // récupère les 20 films les plus populaires
+  const useFetchMovies = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+      const fetchMovies = async() => {
+        const data = await getPopularMovies();
+        console.log(data);
+        setMovies(data.results);
+      };
+  
+      fetchMovies();
+      // pas besoin de teardown ici
+    }, [] );
+  
+    return(movies);
+  };
+  
+  const movies = useFetchMovies();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>Filmopédia</h1>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <input
+          type="text"
+          id="input_nom_film"
+          name="input_nom_film"
+          placeholder="Cherchez un film"
+          value={movieName}
+          onChange = {movieNameChange}
+        />
+        {movieName && <p>Résulatat de la recherche : {movieName}</p>}
+        <h2>Les 20 films les plus populaires</h2>
+        <ul>
+          {movies.map(movie => (
+            <li key={movie.id}>{movie.title}</li>
+          ))}
+        </ul> 
+        {/* <ul>{movies}</ul>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
