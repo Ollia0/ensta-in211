09d2e66de66cf7d2d7efcc3e2f2e199d1a80914a
Ollia0 <dodo.diallo@gmail.com>
2025-05-08 23:39:47 +0200

    refractor: remove old testing features

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,127 +4,108 @@ import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import axios from 'axios';
 import Movie from '../../components/Movie/Movie';
-import Filter from '../../components/Filter/Filter'
+import Filter from '../../components/Filter/Filter';
 
 function Home() {
-
-  const [movieName, setMovieName] = useState('')
-  const [sortCriteria, setSortCriteria] = useState("popularity");
-  const [sortOrder, setSortOrder] = useState("desc")
+  const [movieName, setMovieName] = useState('');
+  const [sortCriteria, setSortCriteria] = useState('popularity');
+  const [sortOrder, setSortOrder] = useState('desc');
   const [minVoteCount, setMinVoteCount] = useState(1);
   const [searchParams, setSearchParams] = useSearchParams();
   const [selectedGenres, setSelectedGenres] = useState(() => {
     const genreParam = searchParams.get('genre');
     return genreParam ? genreParam.split(',').map(Number) : [];
   });
-  
-  // update le nom du film selon l'input
-  const movieNameChange = (Event) => {
-    setMovieName(Event.target.value);
-  }
 
   const getGenres = async () => {
-  try {
-    const response = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${import.meta.env.VITE_API_KEY}`);
-    return response.data.genres;
-  } 
-  catch (error) {
-    console.log("Erreur de chargement des genres");
-    return [];
-  }
-};
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/genre/movie/list?api_key=${import.meta.env.VITE_API_KEY}`,
+      );
+      return response.data.genres;
+    } catch (error) {
+      console.log('Erreur de chargement des genres');
+      return [];
+    }
+  };
 
   const useFetchGenres = () => {
     const [genres, setGenres] = useState([]);
 
     useEffect(() => {
-      const fetchData = async() => {
-        const genresData = await(getGenres());
-        setGenres(genresData)
+      const fetchData = async () => {
+        const genresData = await getGenres();
+        setGenres(genresData);
       };
 
       fetchData();
     }, []);
-    return genres
-  }
+    return genres;
+  };
 
-  const fetchMovies = async (selectedGenres)  => {
+  const fetchMovies = async (selectedGenres) => {
     let url = `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_API_KEY}&sort_by=${sortCriteria}.${sortOrder}&vote_count.gte=${minVoteCount}`;
 
-    if (selectedGenres.length > 0){
+    if (selectedGenres.length > 0) {
       const with_genresParam = selectedGenres.join(',');
       url += `&with_genres=${with_genresParam}`;
     }
 
     try {
       const response = await axios.get(url);
-      return(response.data.results);
-    }
-    catch (error){
-      console.error("Erreur de chargement des films")
+      return response.data.results;
+    } catch (error) {
+      console.error('Erreur de chargement des films');
       return [];
     }
-  }
+  };
 
   const useFetchMovies = (selectedGenres) => {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
-      const fetchData = async() => {
-        const moviesData= await(fetchMovies(selectedGenres));
+      const fetchData = async () => {
+        const moviesData = await fetchMovies(selectedGenres);
         setMovies(moviesData);
       };
 
       fetchData();
     }, [selectedGenres, sortOrder, sortCriteria, minVoteCount]);
     return movies;
-  }
-  
+  };
+
   const genres = useFetchGenres();
-  const movies = useFetchMovies(selectedGenres)
+  const movies = useFetchMovies(selectedGenres);
 
   return (
     <div className="App">
       <header className="App-header">
         {/* partie haute du site, pas le header normal de index */}
-      <div className="header-section">
-        <h1>Filmopédia</h1>
-        <input
-          type="text"
-          id="input_nom_film"
-          name="input_nom_film"
-          placeholder="Cherchez un film"
-          value={movieName}
-          onChange={movieNameChange}
-        />
-        {movieName && <p>Résultat de la recherche : {movieName}</p>}
-      </div>
-
-      <div className="content-section">
-        {/* contenu du site à gauche */}
-        <div className="main-content">
-          <div className="movie-display">
-            {movies.map(movie => (
-              <Movie key={movie.id} movie={movie} genres={genres}/>
-            ))}
+        <h1>Discover</h1>
+        <div className="content-section">
+          {/* contenu du site à gauche */}
+          <div className="main-content">
+            <div className="movie-display">
+              {movies.map((movie) => (
+                <Movie key={movie.id} movie={movie} genres={genres} />
+              ))}
+            </div>
+          </div>
+          {/* panneau filtre à droite*/}
+          <div className="filter-panel">
+            <Filter
+              genres={genres}
+              selectedGenres={selectedGenres}
+              onGenreChange={setSelectedGenres}
+              sortCriteria={sortCriteria}
+              onSortCriteriaChange={setSortCriteria}
+              sortOrder={sortOrder}
+              onSortOrderChange={setSortOrder}
+              minVoteCount={minVoteCount}
+              onMinVoteCountChange={setMinVoteCount}
+            />
           </div>
         </div>
-        {/* panneau filtre à droite*/}
-        <div className="filter-panel">
-          <Filter
-            genres={genres}
-            selectedGenres={selectedGenres}
-            onGenreChange={setSelectedGenres}
-            sortCriteria={sortCriteria}
-            onSortCriteriaChange={setSortCriteria}
-            sortOrder={sortOrder}
-            onSortOrderChange={setSortOrder}
-            minVoteCount={minVoteCount}
-            onMinVoteCountChange={setMinVoteCount}
-            />
-        </div>
-      </div>
-
       </header>
     </div>
   );
