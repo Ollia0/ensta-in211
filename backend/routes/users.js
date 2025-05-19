ffd175f2566bf6adea718ef197bbd4502ea08034
Ollia0 <dodo.diallo@gmail.com>
2025-05-19 21:47:50 +0200

    feat: back route to get another users profile

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,50 +4,24 @@ import User from '../entities/user.js';
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  appDataSource
-    .getRepository(User)
-    .find({})
-    .then(function (users) {
-      res.json({ users: users });
+router.get('/profile/:username', async (req, res) => {
+  try {
+    const { username } = req.params;
+    const userRepository = appDataSource.getRepository(User);
+    const user = await userRepository.findOne({
+      where: { username },
+      select: ['id', 'username'],
     });
-});
 
-router.post('/new', function (req, res) {
-  const userRepository = appDataSource.getRepository(User);
-  const newUser = userRepository.create({
-    email: req.body.email,
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-  });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
 
-  userRepository
-    .insert(newUser)
-    .then(function (newDocument) {
-      res.status(201).json(newDocument);
-    })
-    .catch(function (error) {
-      console.error(error);
-      if (error.code === '23505') {
-        res.status(400).json({
-          message: `User with email "${newUser.email}" already exists`,
-        });
-      } else {
-        res.status(500).json({ message: 'Error while creating the user' });
-      }
-    });
-});
-
-router.delete('/:userId', function (req, res) {
-  appDataSource
-    .getRepository(User)
-    .delete({ id: req.params.userId })
-    .then(function () {
-      res.status(204).json({ message: 'User successfully deleted' });
-    })
-    .catch(function () {
-      res.status(500).json({ message: 'Error while deleting the user' });
-    });
+    return res.json(user);
+  } catch (error) {
+    console.error('Error fetching user profile:', error);
+    return res.status(500).json({ message: 'Server error' });
+  }
 });
 
 export default router;
